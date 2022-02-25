bbcbc32b1376e6951a68c9c10d1aa73636ebd845
Rafael Zardo <rafael.zardo@outlook.com>
2022-02-25 19:56:11 -0300

    Adicionando classe Gerente
    Adicionando Gerente.h

--- Arquivos.h/Gerente.h
+++ Arquivos.h/Gerente.h
@@ -1,4 +1,26 @@
 #ifdef NRW_GERENTE_H
 #define NRW_GERENTE_H
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "Arquivos.h/Funcionario.h"
+
+class Gerente : public Funcionario {
+
+private:
+    bool _curso_superior;
+
+public:
+    Gerente(std::string nome, int codigo, double salario, std::string cpf, std::string endereco, std::string email, std::string data_nascimento, std::string senha, bool curso_superior);
+
+    bool get_curso_superior();
+
+    void set_curso_superior(bool curso_superior);
+
+    virtual exibir_dados() override;
+
+};
+
 #endif 
