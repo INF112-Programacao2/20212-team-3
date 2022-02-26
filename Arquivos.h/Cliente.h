4a909b37bf0242ac43a21e2af52baecdc042b2ca
NataschaPalhares <88913342+NataschaPalhares@users.noreply.github.com>
2022-02-25 21:43:28 -0300

    Update Cliente.h

--- Arquivos.h/Cliente.h
+++ Arquivos.h/Cliente.h
@@ -13,7 +13,21 @@ private:
     int _codigo;
 
 public:
+    Cliente(std::string nome, std::string cpf, std::string telefone, std::string endereco, int codigo);
 
+    std::string get_nome();
+    std::string get_cpf();
+    std::string get_telefone();
+    std::string get_endereco();
+    int get_codigo();
+
+    void set_nome(std::string nome);
+    void set_cpf(std::string cpf);
+    void set_telefone(std::string telefone);
+    void set_endereco(std::string endereco);
+    void set_codigo(int codigo);
+
+    void exibir_dados();
 
 };
 
