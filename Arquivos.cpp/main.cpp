34be0c268dba7d67f18b36ac2cb624002b651e92
Rafael Zardo <rafael.zardo@outlook.com>
2022-02-26 17:10:18 -0300

    Colocando .h no main

---
+++ Arquivos.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Arquivos.h/Atendente.h"
+#include "Arquivos.h/Caixa.h"
+#include "Arquivos.h/Carrinho.h"
+#include "Arquivos.h/Cliente.h"
+#include "Arquivos.h/Estoque.h"
+#include "Arquivos.h/Estoquista.h"
+#include "Arquivos.h/Funcionario.h"
+#include "Arquivos.h/Produto.h"
+#include "Arquivos.h/Venda.h"
+
+int main(void) {
+    
+}
