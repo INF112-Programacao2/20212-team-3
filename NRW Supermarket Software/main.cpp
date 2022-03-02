cfcf7b69a9be05c3225a0fa57585766e76e6d168
NataschaPalhares <88913342+NataschaPalhares@users.noreply.github.com>
2022-03-02 17:39:16 -0300

    Correcao de bugs

--- NRW Supermarket Software/main.cpp
+++ NRW Supermarket Software/main.cpp
@@ -18,10 +18,10 @@
 Estoque estoque;
 std::vector<Cliente> clientes;
 std::vector<Funcionario*> funcionarios;
-std::vector<Gerente> gerentes;
-std::vector<Caixa> caixas;
-std::vector<Estoquista> estoquistas;
-std::vector<Atendente> atendentes;
+std::vector<Gerente*> gerentes;
+std::vector<Caixa*> caixas;
+std::vector<Estoquista*> estoquistas;
+std::vector<Atendente*> atendentes;
 std::vector<Carrinho> carrinhos;
 std::vector<Venda> vendas;
 
@@ -62,36 +62,33 @@ int main(void) {
 
     // Codigos dos gerentes = 1, dos caixas = 2, dos estoquistas = 3, dos atendentes = 4
     // Inicializando os objetos das subclasses de Funcionario
-    
-    // Jeito 1: Nao consegui encontrar solucao para o funcionarios.push_back(*gr1), por isso esta comentado. - RFZ
-    Gerente *gr1 = new Gerente(1, "Roberto Costa", 15000, "245987542-87", "Av. Castelo Branco, 43, Vicosa", "roberto.costa@gmail.com", "02/04/1983", "1roberto", true);
-    gerentes.push_back(*gr1);
-    // funcionarios.push_back(*gr1);
 
     //Jeito 2: Solucao para o jeito 1 - RFZ
-    Gerente gr2(1, "Roberto Costa", 15000, "245987542-87", "Av. Castelo Branco, 43, Vicosa", "roberto.costa@gmail.com", "02/04/1983", "1roberto", true);
-    funcionarios.push_back(&gr2);
+    Gerente gr1(1, "Roberto Costa", 15000, "245987542-87", "Av. Castelo Branco, 43, Vicosa", "roberto.costa@gmail.com", "02/04/1983", "1roberto", true);
+    
+    gerentes.push_back(&gr1);
+    funcionarios.push_back(&gr1);
 
-    Caixa *cx1 = new Caixa(2, "Ana Clara", 3500, "165378930-72", "Av. Brasil, 12, Vicosa", "clara.lispector@gmail.com", "08/04/2000", "2ana", true);
-    Caixa *cx2 = new Caixa(2, "Toli Ramos", 2000, "124528547-01", "Av. PH Rolfs, 56, Vicosa", "toli.ramos@yahoo.com", "20/06/1996", "2toli", false);
+    Caixa cx1(2, "Ana Clara", 3500, "165378930-72", "Av. Brasil, 12, Vicosa", "clara.lispector@gmail.com", "08/04/2000", "2ana", true);
+    Caixa cx2(2, "Toli Ramos", 2000, "124528547-01", "Av. PH Rolfs, 56, Vicosa", "toli.ramos@yahoo.com", "20/06/1996", "2toli", false);
 
-    caixas.push_back(*cx1);
-    caixas.push_back(*cx2);
-    // funcionarios.push_back(*cx1);
-    // funcionarios.push_back(*cx2);
+    caixas.push_back(&cx1);
+    caixas.push_back(&cx2);
+    funcionarios.push_back(&cx1);
+    funcionarios.push_back(&cx2);
 
-    Estoquista *es1 = new Estoquista(3, "Flavinho Pneu", 5000, "646233490-80", "Av. Santa Rita, 101, Vicosa", "flavio.pneu@yahoo.com", "01/01/1991", "3flavio", true);
+    Estoquista es1(3, "Flavinho Pneu", 5000, "646233490-80", "Av. Santa Rita, 101, Vicosa", "flavio.pneu@yahoo.com", "01/01/1991", "3flavio", true);
 
-    estoquistas.push_back(*es1);
-    // funcionarios.push_back(*es1);
+    estoquistas.push_back(&es1);
+    funcionarios.push_back(&es1);
 
-    Atendente *at1 = new Atendente(4, "Pedro Alves", 1200, "342765498-06", "Av. Santo Antonio, 234, Vicosa", "pedro.alves@hotmail.com", "29/02/1992", "3pedro", 20);
-    Atendente *at2 = new Atendente(4, "Maria Catarina", 1350, "062506530-15", "Av. Papa Joao, 322, Vicosa", "maria.catarina@gmail.com", "29/02/1992", "3maria", 20);
+    Atendente at1(4, "Pedro Alves", 1200, "342765498-06", "Av. Santo Antonio, 234, Vicosa", "pedro.alves@hotmail.com", "29/02/1992", "3pedro", 20);
+    Atendente at2(4, "Maria Catarina", 1350, "062506530-15", "Av. Papa Joao, 322, Vicosa", "maria.catarina@gmail.com", "29/02/1992", "3maria", 20);
 
-    atendentes.push_back(*at1);
-    atendentes.push_back(*at2);
-    // funcionarios.push_back(*at1);
-    // funcionarios.push_back(*at2);
+    atendentes.push_back(&at1);
+    atendentes.push_back(&at2);
+    funcionarios.push_back(&at1);
+    funcionarios.push_back(&at2);
 
     // Inicio do Software
     int add_codigo;
